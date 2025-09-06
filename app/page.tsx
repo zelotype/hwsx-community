36db4d1fc41d6020fbfb5f51ab55548b9933f4a0
zelotype <zelotype.3@gmail.com>
2025-09-07 00:14:01 +0700

    edit components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Navbar from "@/Components/Navbar";
-import Hero from "@/Components/Hero";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 import "@/styles/globals.css";
 
 export default function Home() {
